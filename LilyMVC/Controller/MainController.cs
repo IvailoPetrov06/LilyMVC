764962289d00513d324a92b085b62b5413b57be5
IvailoPetrov06 <scriptewepro4@gmail.com>
2023-10-12 20:49:51 +0300

    Done

--- LilyMVC/Controller/MainController.cs
+++ LilyMVC/Controller/MainController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LilyMVC.Model;
+using LilyMVC.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,38 @@ using System.Threading.Tasks;
 
 namespace LilyMVC.Controller
 {
-    class MainController
+    public class MainController
     {
+        private Money model;
+        private MainView view;
+
+        public MainController()
+        {
+            model = new Money();
+            view = new MainView();
+        }
+
+        public void Run()
+        {
+            double totalSavings = 0;
+            double toyMoney = 0;
+
+            for (int age = 1; age <= model.Age; age++)
+            {
+                if (age % 2 == 0)
+                {
+                    totalSavings += (age / 2) * 10.0 - 1.0;
+                    toyMoney += model.ToyPrice;
+                }
+                else
+                {
+                    toyMoney += model.ToyPrice;
+                }
+            }
+
+            totalSavings += toyMoney;
+            model.TotalSavings = totalSavings;
+            view.ShowResult(model.TotalSavings, model.WashingMachinePrice);
+        }
     }
 }
